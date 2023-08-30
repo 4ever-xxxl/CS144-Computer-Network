c1ccfcdceac14e49dc50f5d63b3727f971598340
m1ca <m1ca@cnss.io>
2023-08-30 14:29:25 +0800

    lab2 wrapping_integers part finish

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -5,18 +5,12 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return isn + uint32_t(n); }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +23,24 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t res = uint64_t(n - isn);
+    uint64_t RING = 1ul << 32;
+    if (res >= checkpoint) {
+        uint64_t k = (res - checkpoint) / RING;
+        uint64_t Mod = (res - checkpoint) % RING;
+        if (2ul * Mod > RING && checkpoint >= Mod) {
+            res -= (k + 1ul) * RING;
+        } else {
+            res -= k * RING;
+        }
+    } else {
+        uint64_t k = (checkpoint - res) / RING;
+        uint64_t Mod = (checkpoint - res) % RING;
+        if (2ul * Mod > RING) {
+            res += (k + 1ul) * RING;
+        } else {
+            res += k * RING;
+        }
+    }
+    return res;
 }
